6f79971d65d816c0b4704d75f29c13f27cb0d5a2
Kyle Y <stealingvision@gmail.com>
2016-05-20 12:04:00 -0700

    refactoring

--- js/mainvue.js
+++ js/mainvue.js
@@ -1,16 +1,19 @@
 Vue.component('letterForm', {
 	props: ['list'], //bind list to letter array (v-bind:list="letters")
 	template: '#letter-template', //Assign the template for this object
+	data: function() {
+		return {
+			
+		}
+	},
 	computed: {
-
+		
 	},
 	methods: {
-		addLetter: function(obj) {
-			console.log(obj);
+		addLetter: function(obj) { //add letter to array on keypress
 			this.list.push(new letterObj(String.fromCharCode(obj.charCode)));
 		},
-		deleteLetter: function() {
-			console.log('letter deleted');
+		deleteLetter: function() { //delete letter on backspace
 			this.list.pop();
 		}
 	}
@@ -26,7 +29,7 @@ new Vue({
 
 function letterObj(char) {
 	this.char = char;
-	this.color = '';
+	this.color = 'red';
 	this.font = 'Arial';
 	this.size = "50px";
 	this.background = '';
