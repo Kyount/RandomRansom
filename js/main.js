ba331b71135a4f9250770bbffa2bc3cf823e108f
Kyle Y <stealingvision@gmail.com>
2016-05-19 18:35:50 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+(function() {
+	var app = angular.module('ransom', []);
+
+	app.controller('MessageController', function() {
+
+		this.message = []; //array for cumulation of individual letters
+
+		this.checkForBackspace = function(key) {
+			if (key.keyCode === 8) {
+				this.deleteLetter();
+			}
+		};
+
+		this.addLetter = function(key) { //on keypress
+			var m = setLetterObj(String.fromCharCode(key.charCode)); //transform the letter
+			this.message.push(m); //push the letter to the message
+		};
+
+		this.deleteLetter = function() { //on backspace, remove last letter
+			this.message.pop();
+		};
+
+		var setLetterObj = function(letter) {
+			var n = new letterObj(letter);
+			n.background = getRandomBackgroundColor(n);
+			n.color = randomBoolean(blackOrWhite(), randomizeTextColor(n), 0.5);
+			n.size = "60px";
+			return n;
+		};
+
+		var blackOrWhite = function() {
+			return randomBoolean("#FFFFFF", "#000000", 0.5);
+		};
+
+		var getRandomBackgroundColor = function(n) {
+			var c = n.color;
+			if (n.letter !== " ") {
+
+				if (c === "#FFFFFF") { return randomColor({
+						luminosity: randomBoolean('dark', 'bright', 0.5),
+						hue: randomBoolean('', 'monochrome', 0.7)
+					});
+				} else { return randomColor({
+						luminosity: randomBoolean('light', 'bright', 0.5),
+						hue: randomBoolean('', 'monochrome', 0.7)
+					});
+				}
+			}
+		};
+
+		var randomizeTextColor = function(color) {
+
+		};
+	});
+
+	function letterObj(letter) {
+		this.letter = letter;
+		this.color = '#000000';
+		this.font = 'Arial';
+		this.size = "50px";
+		this.background = '#FFFFFF';
+		this.blend = 1;
+	}
+
+	var randomBoolean = function(x, y, weight) { //randomly selects boolean based on weight (as decimal)
+		if(Math.random()<weight) {
+			return x;
+		} else {
+			return y;
+		}
+	}
+
+	var randomFromList = function(x) { //randomly selects an item from an array
+		var l = x.length;
+		var n = Math.floor(Math.random()*l);
+		console.log(n);
+		for (i=0; i<l; i++) {
+			if (n === i) {
+				return x[i];
+			}
+		}
+	}
+
+})();
